34e3a7b61e8ac50f899145354a584e072a75e186
yourhighness123 <115954865+yourhighness123@users.noreply.github.com>
2024-09-21 21:52:28 +0100

    Add files via upload

---
+++ work.js
@@ -0,0 +1,9 @@
+
+      const hideside = () => {
+        const work = document.querySelector(".sidebar");
+        work.style.display = "none";
+      };
+const openside = () => {
+         const work = document.querySelector(".sidebar");
+         work.style.display = "flex";
+    }
